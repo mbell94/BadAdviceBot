8e2054bff6d118b396e4de1104a077c51a1e325c
mbell94 <bellmattheww@gmail.com>
2016-08-26 16:45:58 -0500

    uploading bare bones

---
+++ ConsoleApplication1/Bot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace ConsoleApplication1
+{
+    class Bot
+    {
+        private DiscordClient bot;
+
+        public void DiscoBot()
+        {
+            bot = new DiscordClient();
+
+
+            bot.MessageReceived += Bot_MessageReceived;
+
+            bot.ExecuteAndWait(async() =>
+            {
+                await bot.Connect("", "");
+            });
+        }
+
+        private static void Bot_MessageReceived(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("{0} said {1}", e.User.Name, e.Message.Text);
+
+            switch (e.Message.Text)
+            {
+                case "!rules":
+                    break;
+                case "who is discobot":
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
