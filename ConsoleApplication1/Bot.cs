0a324513cc4200980be125c07f66b8d4cca82b3f
mbell94 <bellmattheww@gmail.com>
2016-08-28 09:36:05 -0500

    adding rules and findCommand skeleton

--- ConsoleApplication1/Bot.cs
+++ ConsoleApplication1/Bot.cs
@@ -20,23 +20,34 @@ namespace ConsoleApplication1
 
             bot.ExecuteAndWait(async() =>
             {
-                await bot.Connect("", "");
+                await bot.Connect("");
             });
         }
 
-        private static void Bot_MessageReceived(object sender, MessageEventArgs e)
+        private void Bot_MessageReceived(object sender, MessageEventArgs e)
         {
+            if (e.Message.IsAuthor) return;
+
             Console.WriteLine("{0} said {1}", e.User.Name, e.Message.Text);
 
-            switch (e.Message.Text)
+            switch (e.Message.Text.Trim())
             {
                 case "!rules":
+                    e.Channel.SendMessage("Hello, I am bot.  Do not worry about what I do");
                     break;
                 case "who is discobot":
+                    e.Channel.SendMessage(e.User.Mention + " Do not worry about who I am.  Just know I am always here. Watching. Waiting.");
                     break;
                 default:
+                    if (e.Message.Text.IndexOf("/") == 0)
+                        findCommand(e.Message.Text.Trim());
                     break;
             }
+        }
+
+        private void findCommand(string command)
+        {
+
         }
     }
 }
